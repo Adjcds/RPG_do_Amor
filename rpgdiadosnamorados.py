f0f4a6af6b4aaaeadbb786d56659481a8ca2658d
Adrielle Jesus da Cruz Santos <dri.jcds@gmail.com>
2024-05-22 19:43:04 -0300

    Maintaining and improving code functionality.

--- rpgdiadosnamorados.py
+++ rpgdiadosnamorados.py
@@ -1,11 +1,13 @@
 import tkinter as tk
-from tkinter import messagebox
+import turtle
+from tkinter import PhotoImage
 
 def iniciar_janela():
     root = tk.Tk()
     root.title('Dia dos Namorados à Distância')
     root.geometry('800x600')
-    root.configure(background='#000000')
+    root.configure(background='#ffc8dd')
+    criar_botao_coracao(root, exibir_coracao)
     return root
 
 def introducao():
@@ -13,17 +15,23 @@ def introducao():
     texto = "Oii! Você está prestes a embarcar em um dia especial cheio de amor e surpresas, mesmo à distância. Vamos começar!"
     label = tk.Label(root, text=texto, bg='#ffc8dd', fg='#590d22', font=('Montserrat', 16, 'bold'), wraplength=700)
     label.pack(pady=20)
-    botao_proximo = tk.Button(root, text='Começar', bg='#ffb3c1', font=('Montserrat', 12, 'bold'), command=escolha_inicial)
+    botao_proximo = tk.Button(root, text='Começar', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=escolha_inicial)
     botao_proximo.pack(pady=20)
+
+def criar_botao_coracao(master, command):
+    img_coracao = PhotoImage(file="coracao.png")
+    botao_coracao = tk.Button(master, image=img_coracao, command=command, bg='#ffc8dd', bd=0)
+    botao_coracao.image = img_coracao
+    botao_coracao.pack(pady=20)
 
 def escolha_inicial():
     limpar_janela()
     texto = "Você acorda e vê uma mensagem do seu amor desejando um Feliz Dia dos Namorados! Você tem duas opções para começar o dia:"
     label = tk.Label(root, text=texto, bg='#ffc8dd', fg='#590d22', font=('Montserrat', 16, 'bold'), wraplength=700)
     label.pack(pady=20)
-    botao_1 = tk.Button(root, text='Preparar um café da manhã especial juntos em  videochamada.', bg='#ffb3c1', font=('Montserrat', 12, 'bold'), command=cafe_da_manha_virtual)
+    botao_1 = tk.Button(root, text='Preparar um café da manhã especial juntos em videochamada.', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=cafe_da_manha_virtual)
     botao_1.pack(pady=10)
-    botao_2 = tk.Button(root, text='Enviar uma mensagem romântica.', bg='#ffb3c1', font=('Montserrat', 12, 'bold'), command=mensagem_romantica)
+    botao_2 = tk.Button(root, text='Enviar uma mensagem romântica.', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=mensagem_romantica)
     botao_2.pack(pady=10)
 
 def cafe_da_manha_virtual():
@@ -31,9 +39,9 @@ def cafe_da_manha_virtual():
     texto = "Você decide preparar um café da manhã especial na videochamada. O que você vai preparar?"
     label = tk.Label(root, text=texto, bg='#ffc8dd', fg='#590d22', font=('Montserrat', 16, 'bold'), wraplength=700)
     label.pack(pady=20)
-    botao_1 = tk.Button(root, text='Algo simples para ter mais tempo para admirar meu bem', bg='#ffb3c1', font=('Montserrat', 12, 'bold'), command=panquecas)
+    botao_1 = tk.Button(root, text='Algo simples para ter mais tempo para admirar meu bem', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=panquecas)
     botao_1.pack(pady=10)
-    botao_2 = tk.Button(root, text='Caprichar para eu mostrar que eu aprendi muito com masterchef', bg='#ffb3c1', font=('Montserrat', 12, 'bold'), command=torradas)
+    botao_2 = tk.Button(root, text='Caprichar para eu mostrar que eu aprendi muito com masterchef', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=torradas)
     botao_2.pack(pady=10)
 
 def mensagem_romantica():
@@ -41,21 +49,21 @@ def mensagem_romantica():
     texto = "Você decide enviar uma mensagem romântica de bom dia antes da videochamada. O que você vai escrever?"
     label = tk.Label(root, text=texto, bg='#ffc8dd', fg='#590d22', font=('Montserrat', 16, 'bold'), wraplength=700)
     label.pack(pady=20)
-    botao_1 = tk.Button(root, text='Um poema de amor.', bg='#ffb3c1', font=('Montserrat', 12, 'bold'), command=poema)
+    botao_1 = tk.Button(root, text='Um poema de amor.', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=poema)
     botao_1.pack(pady=10)
-    botao_2 = tk.Button(root, text='Uma mensagem curta e doce.', bg='#ffb3c1', font=('Montserrat', 12, 'bold'), command=mensagem_curta)
+    botao_2 = tk.Button(root, text='Uma mensagem curta e doce.', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=mensagem_curta)
     botao_2.pack(pady=10)
 
 def panquecas():
     limpar_janela()
-    texto = "Você preparou café da manhã simple, com seu amor via videochamada!"
+    texto = "Você preparou café da manhã simples, com seu amor via videochamada!"
     label = tk.Label(root, text=texto, bg='#ffc8dd', fg='#590d22', font=('Montserrat', 16, 'bold'), wraplength=700)
     label.pack(pady=20)
     surpresa_na_chamada()
 
 def torradas():
     limpar_janela()
-    texto = "Você um digno café da manhã, com seu amor via videochamada!"
+    texto = "Você preparou um digno café da manhã, com seu amor via videochamada!"
     label = tk.Label(root, text=texto, bg='#ffc8dd', fg='#590d22', font=('Montserrat', 16, 'bold'), wraplength=700)
     label.pack(pady=20)
     surpresa_na_chamada()
@@ -68,8 +76,7 @@ def poema():
                 Num universo de sonhos onde só existe o mel.
 
                 Seus sorrisos são notas de uma doce canção,
-                Que embalam meu coração com pura emoção.
-                Nossas mãos se entrelaçam, formando um laço,
+                Que embalam meu coração com pura emoção.                Nossas mãos se entrelaçam, formando um laço,
                 Numa dança eterna de carinho e abraço.
    
                 Juntos enfrentamos tempestades e vendavais,
@@ -82,39 +89,60 @@ def poema():
                 Que nossa história de amor seja sempre celebrada,
                 Porque ao seu lado, minha vida é uma jornada encantada."""
     
-    label = tk.Label(root, text=texto, bg='black', fg='white', font=('Montserrat', 16, 'bold'), wraplength=700)
+    label = tk.Label(root, text=texto, bg='#ffc8dd', fg='#590d22', font=('Montserrat', 16, 'bold'), wraplength=700)
     label.pack(pady=20)
 
-    cafe_da_manha_virtual()
+    botao_proximo = tk.Button(root, text='Próximo', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=cafe_da_manha_virtual)
+    botao_proximo.pack(pady=20)
 
 def mensagem_curta():
     limpar_janela()
     texto = "Apesar dos desafios que enfrentamos, cada momento ao seu lado é um lembrete do quanto sou abençoada(o) por ter você na minha vida. Seu amor ilumina até os dias mais sombrios e transforma os momentos simples em memórias preciosas. Você é o meu porto seguro, minha fonte de alegria e inspiração. Em cada batida do meu coração, encontro a certeza de que, com você, estou exatamente onde pertenço. Feliz Dia dos Namorados, meu amor!."
     label = tk.Label(root, text=texto, bg='#ffc8dd', fg='#590d22', font=('Montserrat', 16, 'bold'), wraplength=700)
     label.pack(pady=20)
-    cafe_da_manha_virtual()
+
+    botao_proximo = tk.Button(root, text='Próximo', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=cafe_da_manha_virtual)
+    botao_proximo.pack(pady=20)
 
 def surpresa_na_chamada():
+    limpar_janela()
     texto = "Durante a videochamada, você decide fazer uma surpresa. O que você vai fazer?"
     label = tk.Label(root, text=texto, bg='#ffc8dd', fg='#590d22', font=('Montserrat', 16, 'bold'), wraplength=700)
     label.pack(pady=20)
-    botao_1 = tk.Button(root, text='Cantar uma música especial.', bg='#ffb3c1', font=('Montserrat', 12, 'bold'), command=cantar_musica)
+    botao_1 = tk.Button(root, text='Cantar uma música especial.', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=cantar_musica)
     botao_1.pack(pady=10)
-    botao_2 = tk.Button(root, text='Mostrar um presente virtual que você criou.', bg='#ffb3c1', font=('Montserrat', 12, 'bold'), command=presente_virtual)
+    botao_2 = tk.Button(root, text='Mostrar um presente virtual que você criou.',bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=presente_virtual)
     botao_2.pack(pady=10)
-    botao_3 = tk.Button(root, text='Sugerir jogar um game juntos.', bg='#ffb3c1', font=('Montserrat', 12, 'bold'), command=escolher_game)
+    botao_3 = tk.Button(root, text='Sugerir jogar um game juntos.', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=escolher_game)
     botao_3.pack(pady=10)
 
 def cantar_musica():
     limpar_janela()
-    texto = "Você canta uma música especial e seu amor fica emocionado(a)!"
+    texto = """And that's why night after night
+               I'll be fuckin' you right
+               Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday (a week)
+               Monday, Tuesday, Wednesday, Thursday, Friday, seven days a week
+               Every hour, every minute, every second, you know night after night
+               I'll be fuckin' you right seven days a week!"""
+    
     label = tk.Label(root, text=texto, bg='#ffc8dd', fg='#590d22', font=('Montserrat', 16, 'bold'), wraplength=700)
     label.pack(pady=20)
-    final_feliz()
+
+    botao_proximo = tk.Button(root, text='Próximo', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=final_feliz)
+    botao_proximo.pack(pady=20)
 
 def presente_virtual():
     limpar_janela()
-    texto = "Você mostra um presente virtual que você criou e seu amor fica encantado!"
+    texto =""" ◌                             ◌                                       ◌           
+                                            ‧₊ *:･ﾟ彡       ◌                 ☽︎       ◌
+            ◌                                 ✩彡 ･ﾟ *:                                     
+                                ◌                                        ◌
+◌
+                                                 ♡
+                                        (\_(\       /)_/)
+                                       (      )    (      )
+                                     ૮/ʚɞ  |ა      ૮|  ʚɞ\ა
+                                      ( ◌    |      |     ◌ )"""
     label = tk.Label(root, text=texto, bg='#ffc8dd', fg='#590d22', font=('Montserrat', 16, 'bold'), wraplength=700)
     label.pack(pady=20)
     final_feliz()
@@ -124,43 +152,94 @@ def escolher_game():
     texto = "Vocês decidem jogar um game juntos. Qual jogo vocês vão escolher?"
     label = tk.Label(root, text=texto, bg='#ffc8dd', fg='#590d22', font=('Montserrat', 16, 'bold'), wraplength=700)
     label.pack(pady=20)
-    botao_1 = tk.Button(root, text='Fornite.', bg='#ffb3c1', font=('Montserrat', 12, 'bold'), command=jogo_trivia)
+    botao_1 = tk.Button(root, text='Fortnite.',bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=jogo_fortnite)
     botao_1.pack(pady=10)
-    botao_2 = tk.Button(root, text='Valorant', bg='#ffb3c1', font=('Montserrat', 12, 'bold'), command=jogo_aventura)
+    botao_2 = tk.Button(root, text='Valorant', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=jogo_valorant)
     botao_2.pack(pady=10)
-    botao_3 = tk.Button(root, text='Dead by Daylight', bg='#ffb3c1', font=('Montserrat', 12, 'bold'), command=jogo_tabuleiro)
+    botao_3 = tk.Button(root, text='Dead by Daylight', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=jogo_dead_by_daylight)
     botao_3.pack(pady=10)
 
-def jogo_trivia():
+def jogo_fortnite():
     limpar_janela()
-    texto = "Vocês entram no Fortnite e lutam juntos até o final, mostrando um ao outro suas habilidades incríveis e dançam na cara dos inimigos"
+    texto = "Vocês entram no Fortnite e lutam juntos até o final, mostrando um ao outro suas habilidades incríveis e dançam na cara dos inimigos."
     label = tk.Label(root, text=texto, bg='#ffc8dd', fg='#590d22', font=('Montserrat', 16, 'bold'), wraplength=700)
     label.pack(pady=20)
-    final_feliz()
+    botao_proximo = tk.Button(root, text='Próximo', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=final_feliz)
+    botao_proximo.pack(pady=20)
 
-def jogo_aventura():
+def jogo_valorant():
     limpar_janela()
-    texto = "Vocês jogam Valorant, coordenando estratégias e vencendo partidas juntos como uma dupla imbatível"
+    texto = "Vocês jogam Valorant, coordenando estratégias e vencendo partidas juntos como uma dupla imbatível."
     label = tk.Label(root, text=texto, bg='#ffc8dd', fg='#590d22', font=('Montserrat', 16, 'bold'), wraplength=700)
     label.pack(pady=20)
-    final_feliz()
+    botao_proximo = tk.Button(root, text='Próximo', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=final_feliz)
+    botao_proximo.pack(pady=20)
 
-def jogo_tabuleiro():
+def jogo_dead_by_daylight():
     limpar_janela()
     texto = "Vocês jogam Dead by Daylight, trabalhando juntos para sobreviver e escapar dos perigos!"
     label = tk.Label(root, text=texto, bg='#ffc8dd', fg='#590d22', font=('Montserrat', 16, 'bold'), wraplength=700)
     label.pack(pady=20)
-    final_feliz()
+    botao_proximo = tk.Button(root, text='Próximo', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=final_feliz)
+    botao_proximo.pack(pady=20)
 
 def final_feliz():
-    limpar_janela()
-    texto = "Mesmo enfrentando alguns 'game overs', vocês percebem que o amor de vocês é como um jogo infinito, onde cada 'game over' é apenas uma oportunidade para começar de novo, juntos. Juntos, vocês descobrem que o verdadeiro desafio é sempre continuar jogando, não importa o que aconteça. Feliz Dia dos Namorados"
+    texto = "Mesmo enfrentando alguns 'game overs', vocês percebem que o amor de vocês é como um jogo infinito, onde cada 'game over' é apenas uma oportunidade para começar de novo, juntos. Juntos, vocês descobrem que o verdadeiro desafio é sempre continuar jogando, não importa o que aconteça. Feliz Dia dos Namorados!"
     label = tk.Label(root, text=texto, bg='#ffc8dd', fg='#590d22', font=('Montserrat', 16, 'bold'), wraplength=700)
     label.pack(pady=20)
+    
+    botao_voltar = tk.Button(root, text='Voltar ao Início', bg='#ffc8dd', fg='#590d22', font=('Montserrat', 12, 'bold'), command=lambda: [introducao(), exibir_forma_e_texto()])
+    botao_voltar.pack(pady=20)
+
+def exibir_forma_e_texto():
+    t = turtle.Turtle()
+    s = turtle.Turtle()
+
+    t.hideturtle()
+    t.penup()
+    t.goto(0, -10)
+    t.pendown()
+
+    t.pensize(3)
+    t.color("#ffc8dd")
+    t.begin_fill()
+    t.left(140)
+    t.forward(180)
+    t.circle(-90, 200)
+    t.setheading(60)
+    t.circle(-90, 200)
+    t.forward(178)
+    t.end_fill()
+
+    t.penup()
+    t.goto(-130, 130)
+    t.pendown()
+    t.color('#590d22') 
+    texto = "Te amo <3"
+    tamanho_fonte = 25
+    largura_texto = len(texto) * tamanho_fonte / 2
+    t.penup()
+    t.goto(-largura_texto, 130)
+    t.pendown()
+    
+    t.color('#590d22') 
+    t.write(texto, font=("Arial", tamanho_fonte, "bold"))  
+
+    turtle.done()
+ 
+
+    turtle.done()
 
 def limpar_janela():
     for widget in root.winfo_children():
         widget.destroy()
+
+def exibir_coracao():
+    limpar_janela()
+    texto = "Feliz Dia dos Namorados!"
+    label = tk.Label(root, text=texto, bg='#ffc8dd', fg='#590d22', font=('Montserrat', 16, 'bold'))
+    label.pack(pady=20)
+    exibir_forma_e_texto()
 
 root = iniciar_janela()
 introducao()
